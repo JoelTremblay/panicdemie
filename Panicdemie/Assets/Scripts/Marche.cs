22a4978620de71252ee43b412788ed86551bda25
angelicakeys <75345767+angelicakeys@users.noreply.github.com>
2021-03-17 10:10:13 -0400

    marcheTest

---
+++ Panicdemie/Assets/Scripts/Marche.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marche : MonoBehaviour
+{
+
+    let Marche;
+    let Perso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
