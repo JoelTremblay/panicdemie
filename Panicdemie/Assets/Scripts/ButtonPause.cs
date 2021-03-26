fe8784e2723690f9603023937ea6dde8d366b2fa
Feng Jiayi <mua6001@163.com>
2021-03-26 15:24:58 -0400

    add bouton pause

---
+++ Panicdemie/Assets/Scripts/ButtonPause.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPause : MonoBehaviour
+{
+    public GameObject ingameMenu;
+
+    public void OnPause(){ // BOUTON = PAUSE
+        Time.timeScale = 0;
+        ingameMenu.SetActive(true);
+    }
+
+    public void OnResume(){ // BOUTON = JOUER
+        Time.timeScale = 1f;
+        ingameMenu.SetActive(false);
+    }
+
+    public void OnRestart(){ // BOUTON = RESTART
+        //Loading Scene0
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+        Time.timeScale = 1f;
+    }
+
+}
