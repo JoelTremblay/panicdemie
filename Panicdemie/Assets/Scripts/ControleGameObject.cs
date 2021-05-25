cf226448b3aee39356f77b0d11e335539878d832
Feng Jiayi <mua6001@163.com>
2021-05-24 22:40:42 -0400

    correction DontDestroyOnLoad

--- Panicdemie/Assets/Scripts/ControleGameObject.cs
+++ Panicdemie/Assets/Scripts/ControleGameObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 /*
  * Contrôle points
  * Par : Feng Jiayi
@@ -9,13 +10,21 @@ using UnityEngine.UI;
 
 public class ControleGameObject : MonoBehaviour
 {
-   
+  private AudioSource music;
+
+     void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
     void OnMouseDown()
     {
         compteur.Score = compteur.Score + 10;
         compteur.Score2 = compteur.Score2 +1;
+        music.Play();
+
 
         Destroy(this.gameObject);
+       
     }
 
 }
--- Panicdemie/Assets/Scripts/son2.cs
+++ Panicdemie/Assets/Scripts/son2.cs
@@ -9,22 +9,34 @@ using UnityEngine.SceneManagement;
 
 public class son2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+
+    public static son2 Instance { get; set; }//单例
 
     // Update is called once per frame
     void Update()
     {
         if (SceneManager.GetActiveScene().name == "MainScene")
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
         }
         else
         {
-            DontDestroyOnLoad(this.gameObject);
+            Instance = this;
+            DontDestroyOnLoad(this);
         }
+
     }
+
+
 }
